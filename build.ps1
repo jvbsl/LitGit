9ef243c32341d60ba1f940655bb4b23950d44f52
jvbsl <jvbsl@novacyb.com>
2019-12-13 15:34:01 +0100

    Echo branch info for better debugging

--- build.ps1
+++ build.ps1
@@ -6,15 +6,15 @@ $TOOLS_DIR="$TOOL_DIRECTORY/tools"
 
 $NUGET="$TOOLS_DIR/nuget.exe"
 
+New-Item -ItemType Directory -Force -Path $TOOLS_DIR
+
+(new-object System.Net.WebClient).DownloadFile("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe", $NUGET)
+
+
 if(!$IsWindows)
 {
     $ADDITIONAL_ARGS=$NUGET
     $NUGET="mono"
 }
-
-New-Item -ItemType Directory -Force -Path $TOOLS_DIR
-
-(new-object System.Net.WebClient).DownloadFile("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe", $NUGET)
-
 
 &$NUGET $ADDITIONAL_ARGS pack LitGit.nuspec -OutputDirectory output
