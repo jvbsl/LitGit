e53493020cfad554316236104f0128fa458fff6e
jvbsl <jvbsl@novacyb.com>
2019-12-13 14:40:12 +0100

    Corrected powershell to run on Linux as well

--- build.ps1
+++ build.ps1
@@ -2,17 +2,19 @@
 
 $TOOL_DIRECTORY=(split-path -parent $MyInvocation.MyCommand.Definition)
 
-if(!$IsWindows)
-{
-    $MONO="mono"
-}
 $TOOLS_DIR="$TOOL_DIRECTORY/tools"
 
 $NUGET="$TOOLS_DIR/nuget.exe"
+
+if(!$IsWindows)
+{
+    $ADDITIONAL_ARGS=$NUGET
+    $NUGET="mono"
+}
 
 New-Item -ItemType Directory -Force -Path $TOOLS_DIR
 
 (new-object System.Net.WebClient).DownloadFile("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe", $NUGET)
 
 
-&$MONO $NUGET pack LitGit.nuspec -OutputDirectory output
+&$NUGET $ADDITIONAL_ARGS pack LitGit.nuspec -OutputDirectory output
